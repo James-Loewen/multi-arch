d2f2546cecd6efbea769560352d830a27ec8efb7
James Loewen <jamesfloewen@gmail.com>
2023-08-15 14:35:42 -0400

    Add paragraph tag

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def hello_world(request):
-    return HttpResponse("<h1>Hello from Django!</h1>")
+    return HttpResponse("<h1>Hello from Django!</h1><p>This time with paragraph text!</p>")
